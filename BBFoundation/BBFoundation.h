f89baf374749810943503cc86dd8e0ef4107cbe4
William Towe <willbur1984@gmail.com>
2016-11-12 16:18:33 -0500

    Initial Commit

---
+++ BBFoundation/BBFoundation.h
@@ -0,0 +1,19 @@
+//
+//  BBFoundation.h
+//  BBFoundation
+//
+//  Created by William Towe on 11/12/16.
+//  Copyright © 2016 Bion Bilateral, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBFoundation.
+FOUNDATION_EXPORT double BBFoundationVersionNumber;
+
+//! Project version string for BBFoundation.
+FOUNDATION_EXPORT const unsigned char BBFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBFoundation/PublicHeader.h>
+
+
